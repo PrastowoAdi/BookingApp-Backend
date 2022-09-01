804022d77bfb0390f7838941759df969ec92e1e2
prastowoadi <sinaadi75@gmail.com>
2022-09-01 07:23:51 +0700

    First commit

---
+++ index.js
@@ -0,0 +1,24 @@
+import express from "express";
+import dotenv from "dotenv";
+import mongoose from "mongoose";
+
+const app = express();
+dotenv.config();
+
+const connect = async () => {
+  try {
+    await mongoose.connect(process.env.MONGO_URL);
+    console.log("Connected to mongoDB");
+  } catch (error) {
+    throw error;
+  }
+};
+
+mongoose.connection.on("disconnected", () => {
+  console.log("mongoDB disconnected!");
+});
+
+app.listen(4000, () => {
+  connect();
+  console.log("Connected to backend");
+});
